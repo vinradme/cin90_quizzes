9e4fbd6bb5b7cf4fd584374c9c6448709b7173e1
Vinodhini Ravikumar <vinravik@cisco.com>
2015-06-17 16:45:31 -0700

    Added list chapter assignment

---
+++ w1-list-quiz.py
@@ -0,0 +1,13 @@
+# list chapter quiz
+
+x = [1, 2, [3, 5, 6]]
+
+print x[2][2]
+
+list_of_words = ['I', 'love', 'ice', 'cream']
+
+print "".join(list_of_words)
+
+print [1, 2, 3, 4] + [5, 3, 5]
+
+print len([1, 3, 3])
