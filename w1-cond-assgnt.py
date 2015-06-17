a451e39dd3d43a679ab4ea23a98e0619b28a7d49
Vinodhini Ravikumar <vinravik@cisco.com>
2015-06-17 16:07:35 -0700

    Conditionals assignment

---
+++ w1-cond-assgnt.py
@@ -0,0 +1,12 @@
+# Salary vs donation assignment
+
+salary = 10000
+donation = 0
+
+if salary > 1000:
+	donation = (10.0/100) * salary
+	print "You can donate " + str(donation) + " this year!"
+else:
+	print "You won't be able to donate this year!"
+
+
