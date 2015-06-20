a6ec5a32ed61d8dc0e6725c6ada80d30dafcfcff
Vinodhini Ravikumar <vinravik@cisco.com>
2015-06-19 22:57:16 -0700

    Completed tuple assignment

---
+++ w1-tuple-assgnt.py
@@ -0,0 +1,21 @@
+# 1. You can access elements in a tuple using indexing
+# example : a = (3, 6, 5, 7)
+#	    a[0], a[1], a[2], a[3]
+
+#######################################################
+
+# 2. You can convert a tuple to a list using list(tuple)
+# example: a = (3, 6, 5, 7)
+# 	   tuple(a)
+
+#######################################################
+
+# 3. Lists are mutable , but tuples are immutable
+# For example: Operations like (3, 2, 1).sort() will not work on them
+
+#######################################################
+
+# 4. We can fix this operation (1) + (3, 4, 5, 6) by using the below code
+# (1,) + (3, 4, 5, 6) -> (1, 3, 4, 5, 6)
+
+#######################################################
