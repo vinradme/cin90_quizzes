bbd01c69c7698557639ae7d5cb6f3dfd04dd5082
Vinodhini Ravikumar <vinravik@cisco.com>
2015-06-27 23:55:32 -0700

    Added Ackerman function assignment

---
+++ ackerman_func.py
@@ -0,0 +1,7 @@
+def naive_ackermann(m, n):
+   if m == 0:
+       return n + 1
+   elif n == 0:
+       return naive_ackermann(m - 1, 1)
+   else:
+       return naive_ackermann(m - 1, naive_ackermann(m, n - 1))
